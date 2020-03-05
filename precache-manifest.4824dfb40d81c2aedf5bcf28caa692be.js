e289ef709f844f1a0278144174ae11884c4c7c25
Bruno <s4ntosbruno@gmail.com>
2020-03-05 12:17:25 -0300

    Updates

---
+++ precache-manifest.4824dfb40d81c2aedf5bcf28caa692be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c60b4b4d088ed2ab6bf34857942bf84",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "7371e98bb2d279aaaa1d",
+    "url": "/react-to-do/static/css/main.44f4610a.chunk.css"
+  },
+  {
+    "revision": "7039f33b320e18303a81",
+    "url": "/react-to-do/static/js/2.9b19e29b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-to-do/static/js/2.9b19e29b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7371e98bb2d279aaaa1d",
+    "url": "/react-to-do/static/js/main.405de3dc.chunk.js"
+  },
+  {
+    "revision": "5acc2ee765ce30bfd8f4",
+    "url": "/react-to-do/static/js/runtime-main.33603702.js"
+  }
+]);
